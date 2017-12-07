abc954fbba3eae62d8a556a57d37267422fbe6b3
Viktor <viktoryy9494@gmail.com>
2017-12-07 22:44:47 +1000

    Слияние с dev

--- application/src/main/groovy/com/markklim/taxi/drive/app/dao/impl/PriceDao.groovy
+++ application/src/main/groovy/com/markklim/taxi/drive/app/dao/impl/PriceDao.groovy
@@ -5,6 +5,7 @@ import com.datastax.driver.core.querybuilder.Select
 import com.markklim.taxi.drive.app.dao.UniversalDao
 import com.markklim.taxi.drive.app.dao.entity.PriceCtc
 import com.markklim.taxi.drive.app.dao.entity.PriceDtd
+import com.markklim.taxi.drive.app.utils.IdUtil
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.cassandra.core.CassandraTemplate
 import org.springframework.stereotype.Component
@@ -27,7 +28,7 @@ class PriceDao {
 
     Integer getDistrictsRidePrice(String from, String to) {
         Select select = QueryBuilder.select().from('price_dtd')
-        select.where(QueryBuilder.eq('id', generateId(from,to)))
+        select.where(QueryBuilder.eq('id', IdUtil.generateId(from, to)))
 
         // TODO: Exception
         universalDao.selectSingle(select, PriceDtd.class).price
@@ -35,11 +36,7 @@ class PriceDao {
 
     Integer getCitiesRidePrice(String from, String to) {
         Select select = QueryBuilder.select().from('price_ctc')
-        select.where(QueryBuilder.eq('id', generateId(from,to)))
+        select.where(QueryBuilder.eq('id', IdUtil.generateId(from, to)))
         universalDao.selectSingle(select, PriceCtc.class).price
-    }
-
-    private Integer generateId(String from, String to) {
-        [from, to].sort().hashCode()
     }
 }
--- application/src/main/groovy/com/markklim/taxi/drive/app/utils/IdUtil.groovy
+++ application/src/main/groovy/com/markklim/taxi/drive/app/utils/IdUtil.groovy
@@ -1,10 +1,7 @@
 package com.markklim.taxi.drive.app.utils
 
-/**
- * Created by viktor on 25.11.17.
- */
 class IdUtil {
-    static int generateId(String from, String to){
+    static Integer generateId(String from, String to){
         if(!(from && to)){
             throw new IllegalArgumentException("From and To fields" +
                     " must be initialize")
