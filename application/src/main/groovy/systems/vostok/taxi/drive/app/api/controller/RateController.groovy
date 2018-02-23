2fab18c02b286105b763573dc4bf849db3b969a5
MarkKlimenko <22773@bk.ru>
2018-02-23 15:12:01 +1000

    Change multipart form data

--- application/src/main/groovy/systems/vostok/taxi/drive/app/api/controller/RateController.groovy
+++ application/src/main/groovy/systems/vostok/taxi/drive/app/api/controller/RateController.groovy
@@ -9,9 +9,9 @@ import javax.ws.rs.Consumes
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
-import javax.ws.rs.core.MediaType
 
 import static systems.vostok.taxi.drive.app.util.constant.MediaType.APPLICATION_JSON
+import static systems.vostok.taxi.drive.app.util.constant.MediaType.MULTIPART_FORM_DATA
 
 @Controller
 @Path('api')
@@ -22,14 +22,14 @@ class RateController {
 
     @POST
     @Path('price-dtd/upload')
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Consumes(MULTIPART_FORM_DATA)
     uploadDtdConfig(@FormDataParam('file') InputStream file) {
         rateService.uploadDtdConfig(file)
     }
 
     @POST
     @Path('price-ctc/upload')
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Consumes(MULTIPART_FORM_DATA)
     uploadCtcConfig(@FormDataParam('file') InputStream file) {
         rateService.uploadCtcConfig(file)
     }
--- application/src/main/groovy/systems/vostok/taxi/drive/app/util/constant/MediaType.groovy
+++ application/src/main/groovy/systems/vostok/taxi/drive/app/util/constant/MediaType.groovy
@@ -2,5 +2,5 @@ package systems.vostok.taxi.drive.app.util.constant
 
 interface MediaType {
     String APPLICATION_JSON = 'application/json; charset=UTF-8'
-
+    String MULTIPART_FORM_DATA = 'multipart/form-data'
 }
