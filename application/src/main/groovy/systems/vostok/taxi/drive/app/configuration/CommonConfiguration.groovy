7669da5bdf452e1c1000f827b7296327ff4f7d6c
MarkKlimenko <22773@bk.ru>
2018-06-18 00:30:24 +1000

    WIP: Delete redundant imports

--- application/src/main/groovy/systems/vostok/taxi/drive/app/configuration/CommonConfiguration.groovy
+++ application/src/main/groovy/systems/vostok/taxi/drive/app/configuration/CommonConfiguration.groovy
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import systems.vostok.tda.service.DistrictMapperService
 
 @Configuration
