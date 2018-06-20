a38d3354a9165d8c160d9317c6f42b43cb5e6ca9
MarkKlimenko <22773@bk.ru>
2018-06-21 00:46:39 +1000

    Refactor keycloak

--- application/src/main/groovy/systems/vostok/taxi/drive/app/configuration/auth/SsoAuthConfiguration.groovy
+++ application/src/main/groovy/systems/vostok/taxi/drive/app/configuration/auth/SsoAuthConfiguration.groovy
@@ -1,7 +1,6 @@
 package systems.vostok.taxi.drive.app.configuration.auth
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -10,7 +9,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
@@ -21,30 +19,27 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @ConditionalOnProperty(name = 'security.type', havingValue = 'sso')
 class SsoAuthConfiguration extends KeycloakWebSecurityConfigurerAdapter {
     @Autowired
-    public void configureGlobal(
-            AuthenticationManagerBuilder auth) throws Exception {
-
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
-                new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
+    void configureGlobal(AuthenticationManagerBuilder auth) {
+        keycloakAuthenticationProvider().with {
+            it.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper())
+            auth.authenticationProvider(it)
+        }
     }
 
     @Bean
-    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
+    KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        new KeycloakSpringBootConfigResolver()
     }
 
     @Bean
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(
-                new SessionRegistryImpl());
+        new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl())
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+    protected void configure(HttpSecurity http) {
+        super.configure(http)
         http.authorizeRequests()
                 .antMatchers('/**').hasRole('ADMIN')
                 .anyRequest().authenticated()
