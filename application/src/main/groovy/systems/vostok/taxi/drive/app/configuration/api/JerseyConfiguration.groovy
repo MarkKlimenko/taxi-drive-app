5eaebd180223f34b84d3cc022e795f8b369cd575
Mark Klimenko <22773@bk.ru>
2018-08-08 19:42:14 +1000

    Add spring actuator

--- application/src/main/groovy/systems/vostok/taxi/drive/app/configuration/api/JerseyConfiguration.groovy
+++ application/src/main/groovy/systems/vostok/taxi/drive/app/configuration/api/JerseyConfiguration.groovy
@@ -2,9 +2,7 @@ package systems.vostok.taxi.drive.app.configuration.api
 
 import org.glassfish.jersey.media.multipart.MultiPartFeature
 import org.glassfish.jersey.server.ResourceConfig
-import org.glassfish.jersey.servlet.ServletContainer
-import org.springframework.boot.web.servlet.ServletRegistrationBean
-import org.springframework.context.annotation.Bean
+import org.glassfish.jersey.servlet.ServletProperties
 import org.springframework.context.annotation.Configuration
 import systems.vostok.taxi.drive.app.api.controller.*
 import systems.vostok.taxi.drive.app.api.provider.FilterProvider
@@ -12,26 +10,17 @@ import systems.vostok.taxi.drive.app.api.provider.PaginationProvider
 import systems.vostok.taxi.drive.app.api.provider.SearchParametersProvider
 import systems.vostok.taxi.drive.app.api.provider.SortProvider
 
-import static org.glassfish.jersey.servlet.ServletProperties.JAXRS_APPLICATION_CLASS
-
 @Configuration
 class JerseyConfiguration extends ResourceConfig {
     JerseyConfiguration() {
         registerControllers()
         registerComponents()
         registerProviders()
-    }
 
-    @Bean
-    ServletRegistrationBean jerseyServlet() {
-        new ServletRegistrationBean(new ServletContainer(), '/*').with {
-            addInitParameter(JAXRS_APPLICATION_CLASS, JerseyConfiguration.class.getName())
-            it
-        }
+        property(ServletProperties.FILTER_FORWARD_ON_404, true)
     }
 
     private void registerControllers() {
-        register(ServiceController.class)
         register(UniversalCrudController.class)
         register(ClientManagementController.class)
         register(RateController.class)
--- application/src/main/groovy/systems/vostok/taxi/drive/app/util/constant/MediaType.groovy
+++ application/src/main/groovy/systems/vostok/taxi/drive/app/util/constant/MediaType.groovy
@@ -3,6 +3,6 @@ package systems.vostok.taxi.drive.app.util.constant
 class MediaType {
     private MediaType() { throw new UnsupportedOperationException() }
 
-    static final String APPLICATION_JSON = 'application/json; charset=UTF-8'
-    static final String MULTIPART_FORM_DATA = 'multipart/form-data'
+    public static final String APPLICATION_JSON = 'application/json; charset=UTF-8'
+    public static final String MULTIPART_FORM_DATA = 'multipart/form-data'
 }
