3815d8a63c71ca77b4c6dab45017404529cfc4ef
Mark Klimenko <22773@bk.ru>
2018-08-13 16:02:42 +1000

    Implement create client test

--- application/src/test/groovy/systems/vostok/taxi/drive/app/operation/client/ClientFlowTestIntegration.groovy
+++ application/src/test/groovy/systems/vostok/taxi/drive/app/operation/client/ClientFlowTestIntegration.groovy
@@ -6,13 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import systems.vostok.taxi.drive.app.dao.domain.operation.OperationRequest
 import systems.vostok.taxi.drive.app.dao.repository.impl.ClientRepository
-import systems.vostok.taxi.drive.app.executor.OperationService
-
-import static systems.vostok.taxi.drive.app.dao.domain.operation.CoreOperationNames.ADD_CLIENT_OPERATION
-import static systems.vostok.taxi.drive.app.dao.domain.operation.OperationDirections.ENROLL
-import static systems.vostok.taxi.drive.app.test.Dataset.getJsonDataset
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -22,17 +16,14 @@ class ClientFlowTestIntegration {
     ClientRepository clientRepository
 
     @Autowired
-    OperationService operationService
+    ClientFlowTestUtil clientUtil
 
     @Test
     @DisplayName('Create client')
     void createTest() {
-        OperationRequest operationRequest = new OperationRequest(
-                operationName: ADD_CLIENT_OPERATION.name,
-                body: getJsonDataset('client', 'simple_client')
-        )
-
-        operationService.execute(ENROLL, operationRequest)
+        clientUtil.removeAllClients()
+        clientUtil.createClient('simple_client')
+                .with { clientUtil.checkClient('simple_client', it) }
     }
 
     @Test
---
+++ application/src/test/groovy/systems/vostok/taxi/drive/app/operation/client/ClientFlowTestUtil.groovy
@@ -0,0 +1,47 @@
+package systems.vostok.taxi.drive.app.operation.client
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import systems.vostok.taxi.drive.app.dao.domain.operation.OperationRequest
+import systems.vostok.taxi.drive.app.dao.domain.operation.OperationResponse
+import systems.vostok.taxi.drive.app.dao.entity.Client
+import systems.vostok.taxi.drive.app.dao.repository.impl.ClientRepository
+import systems.vostok.taxi.drive.app.executor.OperationService
+
+import static org.junit.jupiter.api.Assertions.assertEquals
+import static systems.vostok.taxi.drive.app.dao.domain.operation.CoreOperationNames.ADD_CLIENT_OPERATION
+import static systems.vostok.taxi.drive.app.dao.domain.operation.OperationDirections.ENROLL
+import static systems.vostok.taxi.drive.app.test.Dataset.getJsonDataset
+
+@Component
+class ClientFlowTestUtil {
+    @Autowired
+    ClientRepository clientRepository
+
+    @Autowired
+    OperationService operationService
+
+    void removeAllClients() {
+        clientRepository.deleteAll()
+    }
+
+    OperationResponse createClient(String detasetName) {
+        OperationRequest operationRequest = new OperationRequest(
+                operationName: ADD_CLIENT_OPERATION.name,
+                body: getJsonDataset('client', detasetName)
+        )
+
+        operationService.execute(ENROLL, operationRequest)
+    }
+
+    void checkClient(String detasetName, OperationResponse response) {
+        Client expectedClient = getJsonDataset('client', detasetName) as Client
+        Client actualResponseClient = response.body as Client
+        Client actualClient = clientRepository.findById(expectedClient.login).get()
+
+        expectedClient.with {
+            assertEquals(it, actualResponseClient)
+            assertEquals(it, actualClient)
+        }
+    }
+}
